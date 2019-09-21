cdcbca139473c7d08ece1ee28c8fbd342aa8cbd4
ishuram <ishuram@gmail.com>
2019-09-20 20:56:41 -0400

    commiting unit test cases

--- Assignment1_Seng8040Final/Assignment1_Seng8040Final_Tests/Class1.cs
+++ Assignment1_Seng8040Final/Assignment1_Seng8040Final_Tests/Class1.cs
@@ -12,16 +12,96 @@ namespace Assignment1_Seng8040Final_Tests
     class Assignment1_Seng8040Final_Tests
     {
         [Test]
-        public void GetLength_input3_expectedLEngthEquals3()
+        public void GetLength_Input99_Returns99()
         {
             //Arrange
-            int l = 3;
-            int w = 4;
-            Rectangle testRectangle = new Rectangle(l, w);
+            int length = 99;
+            int width = 89;
+            Rectangle sampleRectangle = new Rectangle(length, width);
+
             //Act
-            int length = testRectangle.GetLength();
+            int expected= sampleRectangle.GetLength();
+
             //Assert
-            Assert.AreEqual(l, length);
+            Assert.AreEqual(expected, length);
         }
+
+        [Test]
+        public void SetLength_Input1000_Returns1000()
+        {
+            //Arrange
+            int length = 1000;
+            int width = 200;
+            Rectangle sampleRectangle = new Rectangle(length, width);
+
+            //Act
+            int expected = sampleRectangle.SetLength(1000);
+
+            //Assert
+            Assert.AreEqual(expected, length);
+        }
+
+        [Test]
+        public void GetWidth_Input5_Returns5()
+        {
+            //Arrange
+            int length = 20;
+            int width = 5;
+            Rectangle sampleRectangle = new Rectangle(length, width);
+
+            //Act
+            int expected = sampleRectangle.GetWidth();
+
+            //Assert
+            Assert.AreEqual(expected,width);
+        }
+
+        [Test]
+        public void SetWidth_Input900_Returns900()
+        {
+            //Arrange
+            int length = 200;
+            int width = 900;
+            Rectangle sampleRectangle = new Rectangle(length, width);
+
+            //Act
+            int expected = sampleRectangle.SetWidth(900);
+
+            //Assert
+            Assert.AreEqual(expected, width);
+        }
+
+        [Test]
+        public void GetPerimeter_Input20And5_Returns50()
+        {
+            //Arrange
+            int length = 20;
+            int width = 5;
+            Rectangle sampleRectangle = new Rectangle(length, width);
+            int expected = 2 * (length + width);
+
+            //Act
+            int perimeter = sampleRectangle.GetPerimeter();
+
+            //Assert
+            Assert.AreEqual(expected,perimeter);
+        }
+
+        [Test]
+        public void GetArea_Input100And7_Returns700()
+        {
+            //Arrange
+            int l = 20;
+            int w = 5;
+            Rectangle sampleRectangle = new Rectangle(l, w);
+            int expected = l*w;
+
+            //Act
+            int area = sampleRectangle.GetArea();
+
+            //Assert
+            Assert.AreEqual(expected, area);
+        }
+
     }
 }
